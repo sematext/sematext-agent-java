32f6952a3841851995040d45203f02d68e9ec24d
Nedim Šabić <rabbitstack7@gmail.com>
2018-10-22 12:35:03 +0200

    SC-3147: build jar path from provided command name

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -22,44 +22,43 @@ fi
    #JAVA_OPTIONS="$JAVA_OPTIONS -XX:OnOutOfMemoryError=\"kill -9 %p\""
 #fi
 
-if [ -z "$SPM_MONITOR_JAR" ]; then
-  SPM_MONITOR_JAR="$SPM_HOME/spm-monitor/lib/spm-monitor-generic.jar"
-fi
+case $1 in
+     'spm-monitor-generic'|'spm-monitor-redis'|'spm-monitor-haproxy'|'spm-monitor-storm')
+         if [ -z "$JVM_NAME" ]; then
+            JVM_NAME=default
+         fi
 
-if [ "$1" = 'spm-monitor' ]; then
-  if [ -z "$JVM_NAME" ]; then
-     JVM_NAME=default
-  fi	  
+         bash \
+            $SPM_HOME/bin/setup-spm  \
+            --start-agent false \
+            --app-token $APP_TOKEN   \
+            --app-type $APP_TYPE  \
+            --app-subtype $APP_SUBTYPE \
+            --agent-type $AGENT_TYPE \
+            --jvm-name $JVM_NAME \
+            --jmx-params $JMX_PARAMS \
+            --metrics-receiver $METRICS_RECEIVER \
+            --tracing-receiver $TRACE_RECEIVER \
+            --region $REGION \
+            --jmx-host $JMX_HOST \
+            --jmx-port $JMX_PORT \
+            --jmx-pass-file $JMX_PASS_FILE \
+            --jmx-trust-store $JMX_TRUSTSTORE \
+            --jmx-trust-store-pass $JMX_TRUSTSTORE_PASS \
+            $EXTRA_PARAMS
 
-  bash \
-     $SPM_HOME/bin/setup-spm  \
-     --start-agent false \
-     --app-token $APP_TOKEN   \
-     --app-type $APP_TYPE  \
-     --app-subtype $APP_SUBTYPE \
-     --agent-type $AGENT_TYPE \
-     --jvm-name $JVM_NAME \
-     --jmx-params $JMX_PARAMS \
-     --metrics-receiver $METRICS_RECEIVER \
-     --tracing-receiver $TRACE_RECEIVER \
-     --region $REGION \
-     --jmx-host $JMX_HOST \
-     --jmx-port $JMX_PORT \
-     --jmx-pass-file $JMX_PASS_FILE \
-     --jmx-trust-store $JMX_TRUSTSTORE \
-     --jmx-trust-store-pass $JMX_TRUSTSTORE_PASS \
-     $EXTRA_PARAMS
-  
-  if [ $? -ne 0 ]; then
-     exit 1
-  fi
+         if [ $? -ne 0 ]; then
+            exit 1
+         fi
 
-  exec \
-     $JAVA \
-     $SPM_MONITOR_JMX_PARAMS \
-     $JAVA_OPTIONS \
-     -Dspm.home=$SPM_HOME \
-     -cp $SPM_MONITOR_JAR $SPM_MONITOR_STANDALONE_CLASS \
-     $SPM_HOME/spm-monitor/conf/spm-monitor-config-$APP_TOKEN-$JVM_NAME.properties 
-fi
-
+         exec \
+            $JAVA \
+            $SPM_MONITOR_JMX_PARAMS \
+            $JAVA_OPTIONS \
+            -Dspm.home=$SPM_HOME \
+            -cp "$SPM_HOME/spm-monitor/lib/$1.jar" $SPM_MONITOR_STANDALONE_CLASS \
+            $SPM_HOME/spm-monitor/conf/spm-monitor-config-$APP_TOKEN-$JVM_NAME.properties
+            ;;
+     *)
+         exec "$@"
+esac
