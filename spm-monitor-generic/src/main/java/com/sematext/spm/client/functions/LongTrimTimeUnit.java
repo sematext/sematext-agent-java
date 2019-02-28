cac1059ad2d261149e267a6ba636cbc3942edfdf
Sivasamy Kaliappan <sivasamy.kaliappan@sematext.com>
2019-02-28 08:31:45 +0530

    added support for multiple unit strings

--- spm-monitor-generic/src/main/java/com/sematext/spm/client/functions/LongTrimTimeUnit.java
+++ spm-monitor-generic/src/main/java/com/sematext/spm/client/functions/LongTrimTimeUnit.java
@@ -2,26 +2,59 @@ package com.sematext.spm.client.functions;
 
 import com.sematext.spm.client.observation.CalculationFunction;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
  * - LongTrimTimeUnit - Trims the time unit strings (ns, us, ms, s, m, h) from the end of value of the metricName
-     and returns the value converted to specified time unit.
-     E.g. LongTrimUnit(discoveryTime, ms) - trims the time unit from the end of value of metric `discoveryTime` and
-     convert & return the result in milliseconds.
+ * and returns the value converted to specified time unit.
+ * E.g. LongTrimUnit(discoveryTime, ms) - trims the time unit from the end of value of metric `discoveryTime` and
+ * convert & return the result in milliseconds.
  */
 public class LongTrimTimeUnit implements CalculationFunction {
     private static final Map<String, TimeUnit> TIME_UNITS = new LinkedHashMap<String, TimeUnit>();
 
     static {
-        TIME_UNITS.put("ns", TimeUnit.NANOSECONDS);
-        TIME_UNITS.put("us", TimeUnit.MICROSECONDS);
-        TIME_UNITS.put("ms", TimeUnit.MILLISECONDS);
-        TIME_UNITS.put("s", TimeUnit.SECONDS);
-        TIME_UNITS.put("m", TimeUnit.MINUTES);
-        TIME_UNITS.put("h", TimeUnit.HOURS);
+
+        List<StringToTimeUnit> stringToTimeUnits = new ArrayList<StringToTimeUnit>();
+
+        stringToTimeUnits.add(new StringToTimeUnit("nseconds", TimeUnit.NANOSECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("nsecond", TimeUnit.NANOSECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("nsecs", TimeUnit.NANOSECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("nsec", TimeUnit.NANOSECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("ns", TimeUnit.NANOSECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("useconds", TimeUnit.MICROSECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("usecond", TimeUnit.MICROSECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("usecs", TimeUnit.MICROSECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("usec", TimeUnit.MICROSECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("us", TimeUnit.MICROSECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("mseconds", TimeUnit.MILLISECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("msecond", TimeUnit.MILLISECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("msecs", TimeUnit.MILLISECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("msec", TimeUnit.MILLISECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("ms", TimeUnit.MILLISECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("seconds", TimeUnit.SECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("second", TimeUnit.SECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("secs", TimeUnit.SECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("sec", TimeUnit.SECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("s", TimeUnit.SECONDS));
+        stringToTimeUnits.add(new StringToTimeUnit("minutes", TimeUnit.MINUTES));
+        stringToTimeUnits.add(new StringToTimeUnit("minute", TimeUnit.MINUTES));
+        stringToTimeUnits.add(new StringToTimeUnit("mins", TimeUnit.MINUTES));
+        stringToTimeUnits.add(new StringToTimeUnit("min", TimeUnit.MINUTES));
+        stringToTimeUnits.add(new StringToTimeUnit("m", TimeUnit.MINUTES));
+        stringToTimeUnits.add(new StringToTimeUnit("hours", TimeUnit.HOURS));
+        stringToTimeUnits.add(new StringToTimeUnit("hour", TimeUnit.HOURS));
+        stringToTimeUnits.add(new StringToTimeUnit("hrs", TimeUnit.HOURS));
+        stringToTimeUnits.add(new StringToTimeUnit("hr", TimeUnit.HOURS));
+        stringToTimeUnits.add(new StringToTimeUnit("h", TimeUnit.HOURS));
+
+        // Sort the time units by decreasing order of length
+        Collections.sort(stringToTimeUnits, new LengthComparator());
+
+        for (StringToTimeUnit stringToTimeUnit : stringToTimeUnits) {
+            TIME_UNITS.put(stringToTimeUnit.unit, stringToTimeUnit.timeUnit);
+        }
     }
 
     @Override
@@ -41,7 +74,7 @@ public class LongTrimTimeUnit implements CalculationFunction {
 
     private long convert(String value, String metricName, TimeUnit toUnit) {
         for (Map.Entry<String, TimeUnit> fromUnit : TIME_UNITS.entrySet()) {
-            if (value.contains(fromUnit.getKey())) {
+            if (value.endsWith(fromUnit.getKey())) {
                 long time = Long.parseLong(value.substring(0, value.indexOf(fromUnit.getKey())).trim());
                 return toUnit.convert(time, fromUnit.getValue());
             }
@@ -52,5 +85,38 @@ public class LongTrimTimeUnit implements CalculationFunction {
     @Override
     public String calculateTag(Map<String, String> objectNameTags, Object... params) {
         throw new UnsupportedOperationException("Can't be used in tag context");
+    }
+
+    private static final class LengthComparator implements Comparator<StringToTimeUnit> {
+
+        @Override
+        public int compare(StringToTimeUnit o1, StringToTimeUnit o2) {
+            return o2.unit.length() - o1.unit.length();
+        }
+    }
+
+    private static final class StringToTimeUnit {
+        private String unit;
+        private TimeUnit timeUnit;
+
+        StringToTimeUnit(String unit, TimeUnit timeUnit) {
+            this.unit = unit;
+            this.timeUnit = timeUnit;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            StringToTimeUnit that = (StringToTimeUnit) o;
+
+            return unit.equals(that.unit);
+        }
+
+        @Override
+        public int hashCode() {
+            return unit.hashCode();
+        }
     }
 }
--- spm-monitor-generic/src/test/java/com/sematext/spm/client/functions/LongTrimTimeUnitTest.java
+++ spm-monitor-generic/src/test/java/com/sematext/spm/client/functions/LongTrimTimeUnitTest.java
@@ -14,11 +14,17 @@ public class LongTrimTimeUnitTest {
         Map<String,Object> metrics = new HashMap<String, Object>();
         metrics.put("m1","10s");
         metrics.put("m2","10ms");
+        metrics.put("m3","1000mseconds");
+        metrics.put("m4","1hrs");
         long v = (Long) func.calculateAttribute(metrics, "m1", "ms");
-        Assert.assertEquals(v,10000L);
+        Assert.assertEquals(10000L,v);
         v = (Long) func.calculateAttribute(metrics, "m2", "ms");
-        Assert.assertEquals(v,10);
+        Assert.assertEquals(10,v);
         v = (Long) func.calculateAttribute(metrics, "m2", "s");
-        Assert.assertEquals(v,0);
+        Assert.assertEquals(0,v);
+        v = (Long) func.calculateAttribute(metrics, "m3", "s");
+        Assert.assertEquals(1,v);
+        v = (Long) func.calculateAttribute(metrics, "m4", "mins");
+        Assert.assertEquals(60,v);
     }
 }
