81154294c6b7f668c4e160ebe4d56cb68a82b8c0
Bojan Smid <bsmid@sematext.com>
2019-03-27 08:36:47 +0100

    another unit test for json array expressions (in derived attr)

--- spm-monitor/src/test/java/com/sematext/spm/client/observation/JsonExpressionFunctionTest.java
+++ spm-monitor/src/test/java/com/sematext/spm/client/observation/JsonExpressionFunctionTest.java
@@ -23,12 +23,10 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.eclipse.collections.impl.map.mutable.UnifiedMap;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.InputStream;
-import java.util.Map;
 
 import com.sematext.spm.client.http.CachableReliableDataSourceBase;
 import com.sematext.spm.client.json.JsonDataProvider;
@@ -36,61 +34,68 @@ import com.sematext.spm.client.json.JsonDataProvider;
 public class JsonExpressionFunctionTest {
   @Test
   public void test_calculate() {
-    JsonExpressionFunctionForTesting func = new JsonExpressionFunctionForTesting(
+    JsonExpressionFunctionForTesting func = new JsonExpressionFunctionForTesting("es-clusterHealth.json",
         "dummy1", "http://localhost/_cluster/health?format=smile", false, "$.", "valueOfKey:cluster_name");
     Assert.assertEquals("elasticsearch", func.calculate());
 
     // this should produce the same
-    func = new JsonExpressionFunctionForTesting("dummy2", "http://localhost/_cluster/health?format=smile", false,
-        "$.cluster_name", null);
+    func = new JsonExpressionFunctionForTesting("es-clusterHealth.json", "dummy2",
+        "http://localhost/_cluster/health?format=smile", false, "$.cluster_name", null);
     Assert.assertEquals("elasticsearch", func.calculate());
 
-    func = new JsonExpressionFunctionForTesting("dummy3", "http://localhost/_cluster/health?format=smile", false,
-        "$.", "substring_0_4(valueOfKey:cluster_name)");
+    func = new JsonExpressionFunctionForTesting("es-clusterHealth.json", "dummy3",
+        "http://localhost/_cluster/health?format=smile", false, "$.", "substring_0_4(valueOfKey:cluster_name)");
     Assert.assertEquals("elas", func.calculate());
 
-    func = new JsonExpressionFunctionForTesting("dummy4", "http://localhost/_cluster/health?format=smile", false,
-        "$.", "substring_3(valueOfKey:cluster_name)");
+    func = new JsonExpressionFunctionForTesting("es-clusterHealth.json", "dummy4",
+        "http://localhost/_cluster/health?format=smile", false, "$.", "substring_3(valueOfKey:cluster_name)");
     Assert.assertEquals("sticsearch", func.calculate());
 
-    JsonExpressionFunctionForTesting2 func2 = new JsonExpressionFunctionForTesting2(
-        "dummy5", "http://localhost/_cluster/health?format=smile", false, "$.datapoints", "length()");
-    Assert.assertEquals(5, func2.calculate());
+    func = new JsonExpressionFunctionForTesting("json-array.json", "dummy5",
+        "http://localhost/_cluster/health?format=smile", false, "$.datapoints", "length()");
+    Assert.assertEquals(5, func.calculate());
 
-    func2 = new JsonExpressionFunctionForTesting2(
-        "dummy5", "http://localhost/_cluster/health?format=smile", false, "$.datapoints", "avg()");
-    Assert.assertEquals(32d, func2.calculate());
+    func = new JsonExpressionFunctionForTesting("json-array.json", "dummy5",
+        "http://localhost/_cluster/health?format=smile", false, "$.datapoints", "avg()");
+    Assert.assertEquals(32d, func.calculate());
 
-    func2 = new JsonExpressionFunctionForTesting2(
-        "dummy5", "http://localhost/_cluster/health?format=smile", false, "$.datapoints", "sum()");
-    Assert.assertEquals(160d, func2.calculate());
+    func = new JsonExpressionFunctionForTesting("json-array.json", "dummy5",
+        "http://localhost/_cluster/health?format=smile", false, "$.datapoints", "sum()");
+    Assert.assertEquals(160d, func.calculate());
 
-    func2 = new JsonExpressionFunctionForTesting2(
-        "dummy5", "http://localhost/_cluster/health?format=smile", false, "$.datapoints", "max()");
-    Assert.assertEquals(99, func2.calculate());
+    func = new JsonExpressionFunctionForTesting("json-array.json", "dummy5",
+        "http://localhost/_cluster/health?format=smile", false, "$.datapoints", "max()");
+    Assert.assertEquals(99, func.calculate());
 
-    func2 = new JsonExpressionFunctionForTesting2(
-        "dummy5", "http://localhost/_cluster/health?format=smile", false, "$.datapoints[0:3]", "max()");
-    Assert.assertEquals(99, func2.calculate());
+    func = new JsonExpressionFunctionForTesting("json-array.json", "dummy5",
+        "http://localhost/_cluster/health?format=smile", false, "$.datapoints[0:3]", "max()");
+    Assert.assertEquals(99, func.calculate());
 
-    func2 = new JsonExpressionFunctionForTesting2(
-        "dummy5", "http://localhost/_cluster/health?format=smile", false, "$.datapoints[0:3]", "length()");
-    Assert.assertEquals(3, func2.calculate());
+    func = new JsonExpressionFunctionForTesting("json-array.json", "dummy5",
+        "http://localhost/_cluster/health?format=smile", false, "$.datapoints[0:3]", "length()");
+    Assert.assertEquals(3, func.calculate());
     
-    func2 = new JsonExpressionFunctionForTesting2(
-        "dummy5", "http://localhost/_cluster/health?format=smile", false, "$.datapoints", "long(avg())");
-    Assert.assertEquals(32l, func2.calculate());
+    func = new JsonExpressionFunctionForTesting("json-array.json", "dummy5",
+        "http://localhost/_cluster/health?format=smile", false, "$.datapoints", "long(avg())");
+    Assert.assertEquals(32l, func.calculate());
     
-    func2 = new JsonExpressionFunctionForTesting2(
-        "dummy5", "http://localhost/_cluster/health?format=smile", false, "$.datapoints[0:5]", "long(avg())");
-    Assert.assertEquals(32l, func2.calculate());
+    func = new JsonExpressionFunctionForTesting("json-array.json", "dummy5",
+        "http://localhost/_cluster/health?format=smile", false, "$.datapoints[0:5]", "long(avg())");
+    Assert.assertEquals(32l, func.calculate());
+
+    func = new JsonExpressionFunctionForTesting("../json/flink-stats.json", "dummy5",
+        "http://localhost/_cluster/health?format=smile", false, "$.[?(@.id=numRegisteredTaskManagers)].value", null);
+    Assert.assertEquals("2", func.calculate());
   }
 }
 
 class JsonExpressionFunctionForTesting extends JsonExpressionFunction {
-  public JsonExpressionFunctionForTesting(String fullPlaceholderResolvedExpression, String url, boolean smile,
-                                          String jsonDataNodePath, String returnExpression) {
+  private String sourceFile;
+  
+  public JsonExpressionFunctionForTesting(String sourceFile, String fullPlaceholderResolvedExpression, String url,
+      boolean smile, String jsonDataNodePath, String returnExpression) {
     super(fullPlaceholderResolvedExpression, url, smile, jsonDataNodePath, returnExpression, null);
+    this.sourceFile = sourceFile;
   }
 
   @Override
@@ -99,9 +104,9 @@ class JsonExpressionFunctionForTesting extends JsonExpressionFunction {
   }
 
   @Override
-  protected Map<String, Object> getData() {
-    InputStream response = getClass().getResourceAsStream("es-clusterHealth.json");
-    TypeReference<UnifiedMap<String, Object>> typeRef = new TypeReference<UnifiedMap<String, Object>>() {
+  protected Object getData() {
+    InputStream response = getClass().getResourceAsStream(sourceFile);
+    TypeReference<Object> typeRef = new TypeReference<Object>() {
     };
     try {
       return new ObjectMapper(new JsonFactory()).readValue(response, typeRef);
@@ -109,29 +114,4 @@ class JsonExpressionFunctionForTesting extends JsonExpressionFunction {
       throw new RuntimeException(thr);
     }
   }
-}
-
-class JsonExpressionFunctionForTesting2 extends JsonExpressionFunction {
-  public JsonExpressionFunctionForTesting2(String fullPlaceholderResolvedExpression, String url, boolean smile,
-                                          String jsonDataNodePath, String returnExpression) {
-    super(fullPlaceholderResolvedExpression, url, smile, jsonDataNodePath, returnExpression, null);
-  }
-
-  @Override
-  protected CachableReliableDataSourceBase<Object, JsonDataProvider> getDataSource(String url, boolean smile) {
-    return null;
-  }
-
-  @Override
-  protected Map<String, Object> getData() {
-    InputStream response = getClass().getResourceAsStream("json-array.json");
-    TypeReference<UnifiedMap<String, Object>> typeRef = new TypeReference<UnifiedMap<String, Object>>() {
-    };
-    try {
-      return new ObjectMapper(new JsonFactory()).readValue(response, typeRef);
-    } catch (Throwable thr) {
-      throw new RuntimeException(thr);
-    }
-  }
-
 }
